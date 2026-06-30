00112bdedd916858d12c4812428e50311299d4ba
Ala Ebunlomo <ebunlomoala@gmail.com>
2026-06-30 20:00:46 +0100

    Switch from Resend to Gmail SMTP

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,22 @@
-const { Resend } = require('resend');
-const resend = new Resend(process.env.RESEND_API_KEY);
+const nodemailer = require('nodemailer')
 
 const sendEmail = async ({ to, subject, html }) => {
-  const { data, error } = await resend.emails.send({
-    from: process.env.EMAIL_FROM, // must be a verified domain on Resend, or use their default onboarding domain for testing
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: Number(process.env.EMAIL_PORT),
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  })
+
+  await transporter.sendMail({
+    from: process.env.EMAIL_FROM,
     to,
     subject,
     html,
-  });
+  })
+}
 
-  if (error) {
-    console.error('Resend error:', error);
-    throw new Error(error.message);
-  }
-
-  console.log('Email sent:', data.id);
-};
-
-module.exports = sendEmail;
+module.exports = sendEmail
