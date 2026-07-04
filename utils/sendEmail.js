c1ec954c98be5732ee15a9196b3eed29119cfef7
Ala Ebunlomo <ebunlomoala@gmail.com>
2026-07-04 16:15:18 +0100

    Switch Mail Service To Resend

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,27 +1,48 @@
+// const sendEmail = async ({ to, subject, html }) => {
+//   console.log('EMAIL_FROM value:', process.env.EMAIL_FROM);
+//   const response = await fetch('https://api.brevo.com/v3/smtp/email', {
+//     method: 'POST',
+//     headers: {
+//       'accept': 'application/json',
+//       'content-type': 'application/json',
+//       'api-key': process.env.BREVO_API_KEY,
+//     },
+//     body: JSON.stringify({
+//       sender: { email: process.env.EMAIL_FROM, name: 'DLFS' },
+//       to: [{ email: to }],
+//       subject,
+//       htmlContent: html,
+//     }),
+//   });
+
+//   if (!response.ok) {
+//     const error = await response.json();
+//     console.error('Brevo error:', error);
+//     throw new Error(error.message || 'Failed to send email');
+//   }
+
+//   console.log('Email sent successfully to:', to);
+// };
+
+// module.exports = sendEmail;
+
+const { Resend } = require('resend');
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 const sendEmail = async ({ to, subject, html }) => {
-  console.log('EMAIL_FROM value:', process.env.EMAIL_FROM);
-  const response = await fetch('https://api.brevo.com/v3/smtp/email', {
-    method: 'POST',
-    headers: {
-      'accept': 'application/json',
-      'content-type': 'application/json',
-      'api-key': process.env.BREVO_API_KEY,
-    },
-    body: JSON.stringify({
-      sender: { email: process.env.EMAIL_FROM, name: 'DLFS' },
-      to: [{ email: to }],
-      subject,
-      htmlContent: html,
-    }),
+  const { data, error } = await resend.emails.send({
+    from: `DLFS <${process.env.EMAIL_FROM}>`,
+    to,
+    subject,
+    html,
   });
 
-  if (!response.ok) {
-    const error = await response.json();
-    console.error('Brevo error:', error);
-    throw new Error(error.message || 'Failed to send email');
+  if (error) {
+    console.error('Resend error:', error);
+    throw new Error(error.message);
   }
 
-  console.log('Email sent successfully to:', to);
+  console.log('Email sent successfully:', data.id);
 };
 
 module.exports = sendEmail;
